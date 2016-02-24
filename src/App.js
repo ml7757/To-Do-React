efb870660ff042f423e3773b3a3ab40a1e58bf4f
MaurcioGarc1a <maoricio07@gmail.com>
2016-02-24 15:54:02 +0100

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import './stylesheets/components.scss';
 import React from 'react';
-import jQuery from 'jquery';
+//import jQuery from 'jquery';
 import AddTaskForm from './AddTaskForm';
-
 
 class App extends React.Component{
 
   onAddTask(task_value, date_value){
-    console.log("I need to get " + task_value " on " + date_value );
+  //  console.log("I need to get " + task_value " on " + date_value );
   }
 
     render(){
         return (
-          <div>
-          <h1>Check &#9745;</h1>
+          <div className="bio">
+          <img src="Checklogo.png" />
+          <h1 className="head">Check &#9745;</h1>
           <AddTaskForm onHandleData={this.onAddTask.bind(this)} />
           </div>
         );
